bd73e0597ca53c99187ae9b4ef07db6b500ed839
Udaykirankomerla <kududaykiran@gmal.com>
2024-05-23 09:54:12 +0530

    first commit

---
+++ first/first.py
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+# arr=[0,6,8,0,2,1,0]
+# j=1
+# i=0
+# # k=1
+# # for i in range(0,len(arr)-j):
+# while i<len(arr)-j:
+#     if arr[len(arr)-j] == 0:
+#         j+=1
+#
+#     if arr[i]==0:
+#         arr[i],arr[len(arr)-j]=arr[len(arr)-j],arr[i]
+#         j+=1
+#     i+=1
+#
+#
+#
+#
+#
+# for i in range(len(arr)):
+#     print(arr[i],end=" ")
+
+
+# size=int(input("enter the ele : "))
+# arr=[]
+# for i in range(size):
+#     ele=int(input("enter the element : "))
+#     arr.append(ele)
+#
+# def Multiply(int : arr) -> list[int]:
+#     arr2=[]
+#
+#     for i in range(0,len(arr)):
+#         val=1
+#         for j in range(0,len(arr)):
+#             if(arr[j] != arr[i]):
+#                 val *= arr[j]
+#         arr2.append(val)
+#
+#     for i in range(len(arr2)):
+#         print(arr2[i],end=" ")
+#     return arr2
+#
+# print(Multiply(arr))
+
+
